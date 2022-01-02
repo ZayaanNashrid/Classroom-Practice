c2a21ff066bbdd152020997a58182ea2af3fc666
ZayaanNashrid <snashrid161127@bscse.uiu.ac.bd>
2022-01-02 15:59:54 +0600

    add

--- 1.c
+++ 1.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 
+int add(){
+
+    int v;
+    v = 1 + 1;
+    return v;
+
+}
+
 int main(){
-	
+
+    int v;
+
 	printf("Hello World");
-	
-	
+	v = add();
+	printf("\n\n%d\n", v);
+
 }
